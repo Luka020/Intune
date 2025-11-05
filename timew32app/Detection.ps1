cbddd03b5eef6ba73b57dafa743082484814881d
Luka020 <33830178+Luka020@users.noreply.github.com>
2025-11-05 17:57:51 +0100

    Add files via upload

---
+++ timew32app/Detection.ps1
@@ -0,0 +1,11 @@
+$ScheduledTaskName = "Syncthetime" #name of the scheduled task
+
+$Task = Get-ScheduledTask -TaskName $ScheduledTaskName -ErrorAction SilentlyContinue -OutVariable task
+
+If ($null -ne $Task){
+    Write-Output "Scheduled Task Found"
+}
+else {
+    Write-Output "Scheduled Task not Found"
+    Exit 1
+}
---
+++ timew32app/Uninstall.ps1
@@ -0,0 +1,12 @@
+$ScheduledTaskName = "Syncthetime" #name of the scheduled task
+
+$Task = Get-ScheduledTask -TaskName $ScheduledTaskName -ErrorAction SilentlyContinue -OutVariable task
+
+If ($null -ne $Task){
+    Write-Output "Scheduled Task Found"
+    Unregister-ScheduledTask -TaskName $ScheduledTaskName -Confirm:$false
+}
+else {
+    Write-Output "Scheduled Task not Found"
+    Exit 1
+}
